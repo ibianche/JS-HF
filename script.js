a23e8d521e5b30cb90dc693d2176be43e2f13401
ibianche <inna.pomorska@gmail.com>
2022-06-14 18:57:59 +0200

    Gra w okrety R2

--- script.js
+++ script.js
@@ -19,17 +19,19 @@ while (isSunk == false) {
   } else {
     guesses = guesses + 1;
 
-
     if (guess == location1 || guess == location2 || guess == location3) {
+      alert('TRAFIONY!');
       hits = hits + 1;
 
       if (hits == 3) {
         isSunk = true;
         alert('Zatopiłeś mój okręt!');
       }
+    } else {
+      alert('PUDŁO!');
     }
   }
 }
 
-let stats = 'Porzebowałeś' + guesses + ' prób, by zatopić okręt, ' + ' czyli Twoja efektywność wynosi: ' + (3/guesses) + '.';
+let stats = 'Porzebowałeś ' + guesses + ' prób, by zatopić okręt, ' + ' czyli Twoja efektywność wynosi: ' + (3/guesses) + '.';
 alert(stats);
