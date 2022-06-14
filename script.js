583db4e3ffb99bef8d87c9d479a14f4824fcd25d
ibianche <inna.pomorska@gmail.com>
2022-06-14 18:51:02 +0200

    Gra w okręty R2

---
+++ script.js
@@ -0,0 +1,35 @@
+let location1 = 3;
+let location2 = 4;
+let location3 = 5;
+
+
+let guess;
+
+let hits = 0;
+
+let guesses = 0;  //proby
+
+let isSunk = false;
+
+while (isSunk == false) {
+  guess = prompt('Gotów, cel, pal! (podaj liczbę z zakresu od 0-6):');
+
+  if (guesses < 0 || guesses > 6) {
+    alert('Proszę podać prawidłowy numer komórki!');
+  } else {
+    guesses = guesses + 1;
+
+
+    if (guess == location1 || guess == location2 || guess == location3) {
+      hits = hits + 1;
+
+      if (hits == 3) {
+        isSunk = true;
+        alert('Zatopiłeś mój okręt!');
+      }
+    }
+  }
+}
+
+let stats = 'Porzebowałeś' + guesses + ' prób, by zatopić okręt, ' + ' czyli Twoja efektywność wynosi: ' + (3/guesses) + '.';
+alert(stats);
